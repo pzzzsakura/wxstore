7a58b6790a1f3c9b86abeaad1d8550fcf02db65c
maliranXXX <1041766338@qq.com>
2018-08-30 22:10:22 +0800

    'delete'

--- mmns/target/mmns/resources/js/tripaddress.js
+++
@@ -1,9 +0,0 @@
-
-
-mui.ready(function(){
-	
-	var proorderId=localStorage.getItem("proorderId");
-	app.ajaxGet('mmns/mall/querytrack',{proorderId:proorderId},function(data){
-		
-	},function(e){})
-})
